93a7de4a1bd7b11bfcb9dd3eeed59418db00e377
AndreaPotenza <andreapoten3@gmail.com>
2024-10-15 17:20:52 +0200

    aggiunti fizz e buzz

--- js/app.js
+++ js/app.js
@@ -4,6 +4,21 @@
 //multipli di 3 e 5  FizzBuzz
 
 for (let i = 0; i < 100; i++) {
-    let num = i + 1 
-    console.log(num)   
-}
+    
+    let num = i + 1
+    let resto5 = num % 5
+    let resto3 = num % 3
+    let resto2 = (resto5 === 0 && resto3 === 0)
+    
+    
+    if (resto5 === 0) 
+        console.log("Fizz")
+    
+    else if (resto3 === 0)
+        console.log("Buzz")
+    
+    else if (resto2)
+        console.log("FizzBuzz")
+    
+    else console.log(num)       
+}       
