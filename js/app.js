236e44106646b5f0e3df22261a407daefa5d23da
AndreaPotenza <andreapoten3@gmail.com>
2024-10-15 16:23:52 +0200

    primo for

---
+++ js/app.js
@@ -0,0 +1,9 @@
+//stampiamo numeri da 1 a 100
+//multipli di 5 = Fizz
+//multipli di 3 = Buzz
+//multipli di 3 e 5  FizzBuzz
+
+for (let i = 0; i < 100; i++) {
+    let num = i + 1 
+    console.log(num)   
+}
