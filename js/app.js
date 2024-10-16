1dcd15e591bcf9ec24383b0c439434a14642c55f
AndreaPotenza <andreapoten3@gmail.com>
2024-10-16 09:53:16 +0200

    corretto

--- js/app.js
+++ js/app.js
@@ -8,17 +8,17 @@ for (let i = 0; i < 100; i++) {
     let num = i + 1
     let resto5 = num % 5
     let resto3 = num % 3
-    let resto2 = (resto5 === 0 && resto3 === 0)
+    let restoFinale5 = resto5 === 0
+    let restoFinale3 = resto3 === 0
     
-    
-    if (resto5 === 0) 
+    if (restoFinale5 && restoFinale3)
+        console.log("FizzBuzz")
+
+    else if (resto5 === 0) 
         console.log("Fizz")
     
     else if (resto3 === 0)
         console.log("Buzz")
-    
-    else if (resto2)
-        console.log("FizzBuzz")
-    
+
     else console.log(num)       
 }       
